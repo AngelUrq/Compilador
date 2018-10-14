4d54e3e894bb5c45f64ded1ed6ae6632494d09ef
ramaldonadof <romancalle14@gmail.com>
2018-10-14 09:46:45 -0400

    Se agrego el analizador lexico en una clase llamada AL de la cual se extraen datos a traves de un arrraylist, hacia el Form1 en el boton llamado bEjecutar el cual busca un txt para posteriormente analizarlo

--- Compilador/Form1.Designer.cs
+++ Compilador/Form1.Designer.cs
@@ -31,6 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.Editor = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.label1 = new System.Windows.Forms.Label();
@@ -38,12 +39,15 @@
             this.label2 = new System.Windows.Forms.Label();
             this.tabPage4 = new System.Windows.Forms.TabPage();
             this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.rTB_AL = new System.Windows.Forms.RichTextBox();
+            this.dGV1 = new System.Windows.Forms.DataGridView();
+            this.bEjecutar = new System.Windows.Forms.Button();
             this.Editor.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             this.tabPage3.SuspendLayout();
             this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dGV1)).BeginInit();
             this.SuspendLayout();
             // 
             // Editor
@@ -53,9 +57,10 @@
             this.Editor.Controls.Add(this.tabPage3);
             this.Editor.Controls.Add(this.tabPage4);
             this.Editor.Location = new System.Drawing.Point(1, 1);
+            this.Editor.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Editor.Name = "Editor";
             this.Editor.SelectedIndex = 0;
-            this.Editor.Size = new System.Drawing.Size(1297, 805);
+            this.Editor.Size = new System.Drawing.Size(973, 654);
             this.Editor.TabIndex = 0;
             // 
             // tabPage1
@@ -63,28 +68,44 @@
             this.tabPage1.BackColor = System.Drawing.Color.Black;
             this.tabPage1.Controls.Add(this.button1);
             this.tabPage1.Controls.Add(this.richTextBox1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1289, 776);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Size = new System.Drawing.Size(965, 628);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Editor de código";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(816, 102);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 19);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // richTextBox1
             // 
             this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(802, 773);
+            this.richTextBox1.Size = new System.Drawing.Size(602, 629);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.bEjecutar);
+            this.tabPage2.Controls.Add(this.dGV1);
+            this.tabPage2.Controls.Add(this.rTB_AL);
             this.tabPage2.Controls.Add(this.label1);
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1289, 776);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Size = new System.Drawing.Size(965, 628);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Analizador léxico";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -92,18 +113,20 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(568, 31);
+            this.label1.Location = new System.Drawing.Point(426, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(114, 17);
+            this.label1.Size = new System.Drawing.Size(86, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Analizador léxico";
             // 
             // tabPage3
             // 
             this.tabPage3.Controls.Add(this.label2);
-            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(1289, 776);
+            this.tabPage3.Size = new System.Drawing.Size(965, 628);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Analizador sintáctico";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -111,18 +134,20 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(539, 34);
+            this.label2.Location = new System.Drawing.Point(404, 28);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(138, 17);
+            this.label2.Size = new System.Drawing.Size(104, 13);
             this.label2.TabIndex = 0;
             this.label2.Text = "Analizador sintáctico";
             // 
             // tabPage4
             // 
             this.tabPage4.Controls.Add(this.label3);
-            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Size = new System.Drawing.Size(1289, 776);
+            this.tabPage4.Size = new System.Drawing.Size(965, 628);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "Analizador semántico";
             this.tabPage4.UseVisualStyleBackColor = true;
@@ -130,28 +155,48 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(546, 24);
+            this.label3.Location = new System.Drawing.Point(410, 20);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(143, 17);
+            this.label3.Size = new System.Drawing.Size(107, 13);
             this.label3.TabIndex = 0;
             this.label3.Text = "Analizador semántico";
             // 
-            // button1
+            // rTB_AL
             // 
-            this.button1.Location = new System.Drawing.Point(1088, 125);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.rTB_AL.Location = new System.Drawing.Point(36, 90);
+            this.rTB_AL.Name = "rTB_AL";
+            this.rTB_AL.Size = new System.Drawing.Size(448, 484);
+            this.rTB_AL.TabIndex = 1;
+            this.rTB_AL.Text = "";
+            // 
+            // dGV1
+            // 
+            this.dGV1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGV1.Location = new System.Drawing.Point(639, 199);
+            this.dGV1.Name = "dGV1";
+            this.dGV1.Size = new System.Drawing.Size(273, 276);
+            this.dGV1.TabIndex = 2;
+            this.dGV1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dGV1_CellContentClick);
+            // 
+            // bEjecutar
+            // 
+            this.bEjecutar.Location = new System.Drawing.Point(490, 90);
+            this.bEjecutar.Name = "bEjecutar";
+            this.bEjecutar.Size = new System.Drawing.Size(75, 23);
+            this.bEjecutar.TabIndex = 3;
+            this.bEjecutar.Text = "Ejecutar";
+            this.bEjecutar.UseVisualStyleBackColor = true;
+            this.bEjecutar.Click += new System.EventHandler(this.bEjecutar_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1292, 802);
+            this.ClientSize = new System.Drawing.Size(969, 609);
             this.Controls.Add(this.Editor);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
@@ -165,6 +210,7 @@
             this.tabPage3.PerformLayout();
             this.tabPage4.ResumeLayout(false);
             this.tabPage4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dGV1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -181,6 +227,9 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button bEjecutar;
+        private System.Windows.Forms.RichTextBox rTB_AL;
+        public System.Windows.Forms.DataGridView dGV1;
     }
 }
 
--- Compilador/Form1.cs
+++ Compilador/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Collections;
 
 namespace Compilador
 {
@@ -15,6 +17,35 @@ namespace Compilador
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void bEjecutar_Click(object sender, EventArgs e)
+        {
+            AL al = new AL();
+            OpenFileDialog abrir = new OpenFileDialog();
+            abrir.Filter = "Documento de texto|*.txt";
+            abrir.Title = "Guardar RichTextBox";
+            //abrir.FileName = "Prueba";
+            var resultado = abrir.ShowDialog();
+            if (resultado == DialogResult.OK)
+            {
+                dGV1.Columns.Add("codigo", "Codigo");
+                dGV1.Columns.Add("Tipo", "Tipo");
+                StreamReader leer = new StreamReader(abrir.FileName);
+                rTB_AL.Text = leer.ReadToEnd();
+                String ass = rTB_AL.Text;
+                al.Separar(ass);
+                for (int i = 0; i<al.palabra.Count; i++)
+                {
+                    dGV1.Rows.Add(al.palabra[i],al.tipoda[i]);
+                }
+                leer.Close();
+            }
+        }
+
+        public void dGV1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
