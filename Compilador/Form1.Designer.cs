e3014c8b8f0c60e087bfdc72197affd715d0f4d0
Diego <dugotcharlot@gmail.com>
2018-10-16 14:36:01 -0400

    Arbol de Derivacion añadido

--- Compilador/Form1.Designer.cs
+++ Compilador/Form1.Designer.cs
@@ -31,6 +31,7 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.Editor = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.label1 = new System.Windows.Forms.Label();
@@ -38,12 +39,15 @@
             this.label2 = new System.Windows.Forms.Label();
             this.tabPage4 = new System.Windows.Forms.TabPage();
             this.label3 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TreeBtn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.Editor.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
             this.tabPage3.SuspendLayout();
             this.tabPage4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // Editor
@@ -53,9 +57,10 @@
             this.Editor.Controls.Add(this.tabPage3);
             this.Editor.Controls.Add(this.tabPage4);
             this.Editor.Location = new System.Drawing.Point(1, 1);
+            this.Editor.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Editor.Name = "Editor";
             this.Editor.SelectedIndex = 0;
-            this.Editor.Size = new System.Drawing.Size(1297, 805);
+            this.Editor.Size = new System.Drawing.Size(973, 654);
             this.Editor.TabIndex = 0;
             // 
             // tabPage1
@@ -63,28 +68,41 @@
             this.tabPage1.BackColor = System.Drawing.Color.Black;
             this.tabPage1.Controls.Add(this.button1);
             this.tabPage1.Controls.Add(this.richTextBox1);
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1289, 776);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Size = new System.Drawing.Size(965, 628);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Editor de código";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(816, 102);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(56, 19);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // richTextBox1
             // 
             this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(802, 773);
+            this.richTextBox1.Size = new System.Drawing.Size(602, 629);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
             // 
             // tabPage2
             // 
             this.tabPage2.Controls.Add(this.label1);
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1289, 776);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Size = new System.Drawing.Size(965, 628);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Analizador léxico";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -92,18 +110,23 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(568, 31);
+            this.label1.Location = new System.Drawing.Point(426, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(114, 17);
+            this.label1.Size = new System.Drawing.Size(86, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Analizador léxico";
             // 
             // tabPage3
             // 
+            this.tabPage3.Controls.Add(this.textBox1);
+            this.tabPage3.Controls.Add(this.TreeBtn);
+            this.tabPage3.Controls.Add(this.pictureBox1);
             this.tabPage3.Controls.Add(this.label2);
-            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Size = new System.Drawing.Size(1289, 776);
+            this.tabPage3.Size = new System.Drawing.Size(965, 628);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Analizador sintáctico";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -111,18 +134,20 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(539, 34);
+            this.label2.Location = new System.Drawing.Point(404, 28);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(138, 17);
+            this.label2.Size = new System.Drawing.Size(104, 13);
             this.label2.TabIndex = 0;
             this.label2.Text = "Analizador sintáctico";
             // 
             // tabPage4
             // 
             this.tabPage4.Controls.Add(this.label3);
-            this.tabPage4.Location = new System.Drawing.Point(4, 25);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage4.Name = "tabPage4";
-            this.tabPage4.Size = new System.Drawing.Size(1289, 776);
+            this.tabPage4.Size = new System.Drawing.Size(965, 628);
             this.tabPage4.TabIndex = 3;
             this.tabPage4.Text = "Analizador semántico";
             this.tabPage4.UseVisualStyleBackColor = true;
@@ -130,28 +155,46 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(546, 24);
+            this.label3.Location = new System.Drawing.Point(410, 20);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(143, 17);
+            this.label3.Size = new System.Drawing.Size(107, 13);
             this.label3.TabIndex = 0;
             this.label3.Text = "Analizador semántico";
             // 
-            // button1
+            // pictureBox1
             // 
-            this.button1.Location = new System.Drawing.Point(1088, 125);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.pictureBox1.Location = new System.Drawing.Point(227, 44);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(737, 587);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // TreeBtn
+            // 
+            this.TreeBtn.Location = new System.Drawing.Point(18, 536);
+            this.TreeBtn.Name = "TreeBtn";
+            this.TreeBtn.Size = new System.Drawing.Size(188, 27);
+            this.TreeBtn.TabIndex = 2;
+            this.TreeBtn.Text = "Parse Tree";
+            this.TreeBtn.UseVisualStyleBackColor = true;
+            this.TreeBtn.Click += new System.EventHandler(this.TreeBtn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(18, 467);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(188, 20);
+            this.textBox1.TabIndex = 3;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1292, 802);
+            this.ClientSize = new System.Drawing.Size(969, 652);
             this.Controls.Add(this.Editor);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
@@ -165,6 +208,7 @@
             this.tabPage3.PerformLayout();
             this.tabPage4.ResumeLayout(false);
             this.tabPage4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -181,6 +225,9 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button TreeBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Compilador/Form1.cs
+++ Compilador/Form1.cs
@@ -12,9 +12,117 @@ namespace Compilador
 {
     public partial class Form1 : Form
     {
+
+        string value = "";
+
+        char[] entry;
+        char[] NonTerminals = {'A','B','C','D','E','F','G','H','I','J','K','L',
+                               'M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+        string B1 = "abc";
+        char[] B;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void TreeBtn_Click(object sender, EventArgs e)
+        {
+            value = textBox1.Text;
+            entry = value.ToCharArray();
+            B = B1.ToCharArray();
+
+            Bitmap surface = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics GFX = Graphics.FromImage(surface);
+
+            Rectangle rectangulo0 = new Rectangle((surface.Width / 2) - 50, 0, 50, 50);
+
+            GFX.DrawEllipse(Pens.Black, rectangulo0);
+            GFX.FillEllipse(Brushes.LightGreen, rectangulo0);
+
+            for (int i = 0; i < entry.Length; i++)
+            {
+
+                if (entry.Length == 1)
+                {
+                    Rectangle rectangulo1 = new Rectangle((surface.Width / 2) - 50, 100, 50, 50);
+
+                    GFX.DrawLine(Pens.Black, rectangulo0.Right - rectangulo0.Width / 2, rectangulo0.Bottom, rectangulo1.Right - rectangulo1.Width / 2, rectangulo1.Top);
+                    GFX.DrawEllipse(Pens.Black, rectangulo1);
+                    GFX.FillEllipse(Brushes.Green, rectangulo1);
+                    using (Font font = new Font("Arial", 14, FontStyle.Regular, GraphicsUnit.Pixel))
+                    {
+                        StringFormat stringFormat = new StringFormat();
+                        stringFormat.Alignment = StringAlignment.Center;
+                        stringFormat.LineAlignment = StringAlignment.Center;
+
+                        GFX.DrawString(entry[i].ToString(), font, Brushes.Black, rectangulo1, stringFormat);
+
+                    }
+                }
+                else
+                {
+                    Rectangle rectangulo2 = new Rectangle(((surface.Width - 100) * i) / (entry.Length - 1), 100, 50, 50);
+
+                    GFX.DrawLine(Pens.Black, rectangulo0.Right - rectangulo0.Width / 2, rectangulo0.Bottom, rectangulo2.Right - rectangulo2.Width / 2, rectangulo2.Top);
+                    GFX.DrawEllipse(Pens.Black, rectangulo2);
+                    GFX.FillEllipse(Brushes.Green, rectangulo2);
+                    using (Font font = new Font("Arial", 12, FontStyle.Regular, GraphicsUnit.Point))
+                    {
+                        StringFormat stringFormat = new StringFormat();
+                        stringFormat.Alignment = StringAlignment.Center;
+                        stringFormat.LineAlignment = StringAlignment.Center;
+
+                        GFX.DrawString(entry[i].ToString(), font, Brushes.Black, rectangulo2, stringFormat);
+                    }
+                }
+                if (NonTerm(entry[i]))
+                {
+                    for (int j = 0; j < B.Length; j++)
+                    {
+                        if (B.Length == 1)
+                        {
+
+                            Rectangle rectangulo2 = new Rectangle(((surface.Width - 100) * i) / (entry.Length - 1), 100, 50, 50);
+                            Rectangle rectangulo3 = new Rectangle(((surface.Width - 100) * i) / (entry.Length - 1), 200, 50, 50);
+                            GFX.DrawLine(Pens.Black, rectangulo2.Right - rectangulo2.Width / 2, rectangulo2.Bottom, rectangulo3.Right - rectangulo3.Width / 2, rectangulo3.Top);
+                            GFX.DrawEllipse(Pens.Black, rectangulo3);
+                            GFX.FillEllipse(Brushes.Green, rectangulo3);
+                            using (Font font = new Font("Arial", 12, FontStyle.Regular, GraphicsUnit.Point))
+                            {
+                                StringFormat stringFormat = new StringFormat();
+                                stringFormat.Alignment = StringAlignment.Center;
+                                stringFormat.LineAlignment = StringAlignment.Center;
+
+                                GFX.DrawString(B[j].ToString(), font, Brushes.Black, rectangulo3, stringFormat);
+                            }
+
+                        }
+                        else
+                        {
+                            Rectangle rectangulo2 = new Rectangle(((surface.Width - 100) * i) / (entry.Length - 1), 100, 50, 50);
+                            Rectangle rectangulo4 = new Rectangle(((surface.Width - 100) * j) / (B.Length - 1), 200, 50, 50);
+
+                            GFX.DrawLine(Pens.Black, rectangulo2.Right - rectangulo2.Width / 2, rectangulo2.Bottom, rectangulo4.Right - rectangulo4.Width / 2, rectangulo4.Top);
+                            GFX.DrawEllipse(Pens.Black, rectangulo4);
+                            GFX.FillEllipse(Brushes.Green, rectangulo4);
+                            using (Font font = new Font("Arial", 12, FontStyle.Regular, GraphicsUnit.Point))
+                            {
+                                StringFormat stringFormat = new StringFormat();
+                                stringFormat.Alignment = StringAlignment.Center;
+                                stringFormat.LineAlignment = StringAlignment.Center;
+
+                                GFX.DrawString(B[j].ToString(), font, Brushes.Black, rectangulo4, stringFormat);
+                            }
+                        }
+                    }
+                }
+            }
+            pictureBox1.Image = surface;
+        }
+        public Boolean NonTerm(char palabra)
+        {
+            return NonTerminals.Contains(palabra);
         }
     }
 }
