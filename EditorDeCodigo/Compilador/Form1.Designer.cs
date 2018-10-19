5f331376acbebce5410b648b0eb3cd2e9a98fe1d
Rodrigo Zambrana <williamzambrana85@gmail.com>
2018-10-19 17:08:10 -0400

    Editor de Codigo con Lineas
    Muestra numero de Linea en el editor de codigo

---
+++ EditorDeCodigo/Compilador/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Compilador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Editor = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.LineNumberTextBox = new System.Windows.Forms.RichTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.bEjecutar = new System.Windows.Forms.Button();
+            this.dGV1 = new System.Windows.Forms.DataGridView();
+            this.rTB_AL = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Editor.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dGV1)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Editor
+            // 
+            this.Editor.Controls.Add(this.tabPage1);
+            this.Editor.Controls.Add(this.tabPage2);
+            this.Editor.Controls.Add(this.tabPage3);
+            this.Editor.Controls.Add(this.tabPage4);
+            this.Editor.Location = new System.Drawing.Point(1, -1);
+            this.Editor.Margin = new System.Windows.Forms.Padding(2);
+            this.Editor.Name = "Editor";
+            this.Editor.SelectedIndex = 0;
+            this.Editor.Size = new System.Drawing.Size(1148, 615);
+            this.Editor.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.BackColor = System.Drawing.Color.White;
+            this.tabPage1.Controls.Add(this.LineNumberTextBox);
+            this.tabPage1.Controls.Add(this.richTextBox1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage1.Size = new System.Drawing.Size(1140, 589);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Editor de código";
+            // 
+            // LineNumberTextBox
+            // 
+            this.LineNumberTextBox.BackColor = System.Drawing.Color.Gainsboro;
+            this.LineNumberTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.LineNumberTextBox.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LineNumberTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LineNumberTextBox.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.LineNumberTextBox.Location = new System.Drawing.Point(2, 2);
+            this.LineNumberTextBox.Name = "LineNumberTextBox";
+            this.LineNumberTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.LineNumberTextBox.Size = new System.Drawing.Size(24, 585);
+            this.LineNumberTextBox.TabIndex = 3;
+            this.LineNumberTextBox.Text = "";
+            this.LineNumberTextBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.LineNumberTextBox_MouseDown);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(32, 0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(712, 587);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.SelectionChanged += new System.EventHandler(this.richTextBox1_SelectionChanged);
+            this.richTextBox1.VScroll += new System.EventHandler(this.richTextBox1_VScroll);
+            this.richTextBox1.FontChanged += new System.EventHandler(this.richTextBox1_FontChanged);
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.bEjecutar);
+            this.tabPage2.Controls.Add(this.dGV1);
+            this.tabPage2.Controls.Add(this.rTB_AL);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2);
+            this.tabPage2.Size = new System.Drawing.Size(1140, 589);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Analizador léxico";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // bEjecutar
+            // 
+            this.bEjecutar.Location = new System.Drawing.Point(641, 68);
+            this.bEjecutar.Name = "bEjecutar";
+            this.bEjecutar.Size = new System.Drawing.Size(178, 25);
+            this.bEjecutar.TabIndex = 3;
+            this.bEjecutar.Text = "Buscar Archivo";
+            this.bEjecutar.UseVisualStyleBackColor = true;
+            this.bEjecutar.Click += new System.EventHandler(this.bEjecutar_Click);
+            // 
+            // dGV1
+            // 
+            this.dGV1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dGV1.Location = new System.Drawing.Point(550, 99);
+            this.dGV1.Name = "dGV1";
+            this.dGV1.Size = new System.Drawing.Size(407, 446);
+            this.dGV1.TabIndex = 2;
+            // 
+            // rTB_AL
+            // 
+            this.rTB_AL.Location = new System.Drawing.Point(51, 61);
+            this.rTB_AL.Name = "rTB_AL";
+            this.rTB_AL.Size = new System.Drawing.Size(448, 484);
+            this.rTB_AL.TabIndex = 1;
+            this.rTB_AL.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(426, 25);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Analizador léxico";
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.label2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(2);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(1060, 630);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Analizador sintáctico";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(404, 28);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Analizador sintáctico";
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.label3);
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Margin = new System.Windows.Forms.Padding(2);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(1060, 630);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Analizador semántico";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(410, 20);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(107, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Analizador semántico";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1146, 609);
+            this.Controls.Add(this.Editor);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "XQCode";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.Editor.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dGV1)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl Editor;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button bEjecutar;
+        private System.Windows.Forms.RichTextBox rTB_AL;
+        public System.Windows.Forms.DataGridView dGV1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.RichTextBox LineNumberTextBox;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ EditorDeCodigo/Compilador/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections;
+
+namespace Compilador
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int getWidth()
+        {
+            int w = 25;
+            // get total lines of richTextBox1    
+            int line = richTextBox1.Lines.Length;
+
+            if (line <= 99)
+            {
+                w = 20 + (int)richTextBox1.Font.Size;
+            }
+            else if (line <= 999)
+            {
+                w = 30 + (int)richTextBox1.Font.Size;
+            }
+            else
+            {
+                w = 50 + (int)richTextBox1.Font.Size;
+            }
+
+            return w;
+        }
+
+        public void AddLineNumbers()
+        {
+            Point pt = new Point(0, 0);
+            int First_Index = richTextBox1.GetCharIndexFromPosition(pt);
+            int First_Line = richTextBox1.GetLineFromCharIndex(First_Index);
+            pt.X = ClientRectangle.Width;
+            pt.Y = ClientRectangle.Height;
+            int Last_Index = richTextBox1.GetCharIndexFromPosition(pt);
+            int Last_Line = richTextBox1.GetLineFromCharIndex(Last_Index);
+            LineNumberTextBox.SelectionAlignment = HorizontalAlignment.Center;
+            LineNumberTextBox.Text = "";
+            LineNumberTextBox.Width = getWidth();
+            for (int i = First_Line; i <= Last_Line + 1; i++)
+            {
+                LineNumberTextBox.Text += i + 1 + "\n";
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LineNumberTextBox.Font = richTextBox1.Font;
+            richTextBox1.Select();
+            AddLineNumbers();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            AddLineNumbers();
+        }
+
+        private void richTextBox1_SelectionChanged(object sender, EventArgs e)
+        {
+            Point pt = richTextBox1.GetPositionFromCharIndex(richTextBox1.SelectionStart);
+            if (pt.X == 1)
+            {
+                AddLineNumbers();
+            }
+        }
+
+        private void richTextBox1_VScroll(object sender, EventArgs e)
+        {
+            LineNumberTextBox.Text = "";
+            AddLineNumbers();
+            LineNumberTextBox.Invalidate();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text == "")
+            {
+                AddLineNumbers();
+            }
+        }
+
+        private void richTextBox1_FontChanged(object sender, EventArgs e)
+        {
+            LineNumberTextBox.Font = richTextBox1.Font;
+            richTextBox1.Select();
+            AddLineNumbers();
+        }
+
+        private void LineNumberTextBox_MouseDown(object sender, MouseEventArgs e)
+        {
+            richTextBox1.Select();
+            LineNumberTextBox.DeselectAll();
+        }
+
+        private void bEjecutar_Click(object sender, EventArgs e)
+        {
+            AL al = new AL();
+            OpenFileDialog abrir = new OpenFileDialog();
+            abrir.Filter = "Documento de texto|*.txt";
+            abrir.Title = "Guardar RichTextBox";
+            //abrir.FileName = "Prueba";
+            var resultado = abrir.ShowDialog();
+            if (resultado == DialogResult.OK)
+            {
+                dGV1.Columns.Add("Fila", "Fila");
+                dGV1.Columns.Add("Columna", "Columna");
+                dGV1.Columns.Add("codigo", "Codigo");
+                dGV1.Columns.Add("Tipo", "Tipo");
+                StreamReader leer = new StreamReader(abrir.FileName);
+                rTB_AL.Text = leer.ReadToEnd();
+                String cadena1 = rTB_AL.Text;
+                al.Separar(cadena1);
+                int fi = 1;
+                int col = 1;
+                for (int i = 0; i < al.palabra.Count; i++)
+                {
+                    if (al.palabra[i].Equals("#$") && al.tipoda[i].Equals("Salto"))
+                    {
+                        fi++;
+                        col = 1;
+                    }
+                    else
+                    {
+                        dGV1.Rows.Add(fi, col, al.palabra[i], al.tipoda[i]);
+                        col++;
+                    }
+                }
+                leer.Close();
+            }
+        }
+    }
+}
